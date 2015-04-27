236f969bf40a738b7ad09737911c1db009027465
Liz Burton <liz.burton@comcast.net>
2015-04-26 22:54:33 -0400

    Home page content

--- wp-content/themes/seniorsoutdoors/functions.php
+++ wp-content/themes/seniorsoutdoors/functions.php
@@ -46,7 +46,7 @@ function seniorsoutdoors_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
@@ -107,6 +107,8 @@ function seniorsoutdoors_scripts() {
 	wp_enqueue_script( 'seniorsoutdoors-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'seniorsoutdoors-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	wp_enqueue_script( 'jquery', false, false, false, false );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- wp-content/themes/seniorsoutdoors/header.php
+++ wp-content/themes/seniorsoutdoors/header.php
@@ -11,6 +11,13 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script type="text/javascript" src="/liz_finalproject_wp/wp-content/themes/seniorsoutdoors/js/jquery.flexslider.js"></script>
+<script type="text/javascript" charset="utf-8">
+$(window).load(function() {
+	$('.flexslider').flexslider();
+});
+</script> 
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
@@ -24,7 +31,8 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<img class="logo" src="http://localhost:8888/liz_finalproject_wp/wp-content/themes/seniorsoutdoors/images/so_logo_small.png" alt="Seniors Outdoors logo">
-		</div><!-- .site-branding -->
+<!--			<img class="logo" src="http://burtonux.com/liz_finalproject_wp/wp-content/themes/seniorsoutdoors/images/so_logo_small.png" alt="Seniors Outdoors logo">
+-->		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'seniorsoutdoors' ); ?></button>
